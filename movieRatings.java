c9440c3c974a15bb72698f47456d5cb58c449aa5
mtho9 <144266272+mtho9@users.noreply.github.com>
2023-11-08 14:52:52 -0500

    movieRatings.java
    Mandy's task

---
+++ movieRatings.java
@@ -0,0 +1,55 @@
+package lab7;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+/**
+ * Contains method to rank movies based on popular ties among students
+ * @author mandy.ho
+ */
+public class movieRatings {
+
+private static void rankMovies (TreeMap<String, TreeSet<String>>)	 {
+	Arraylist rankedList = new Arraylist(); // creating empty list where I will populate the most popular movie first
+	// initalize a count for how many times a movie is popping up
+	int mostHighlyRated = 0;
+	//read the csv file
+TreeMap<name, TreeSet<String> favoriteMovie>	= readFileToTreeMap (Movies.csv);
+
+
+	//for loop after [0] are movies
+while (!favoriteMovie = null) {
+
+	for (int i = 0; i<favoriteMovie.length; i++) {
+		//use values collection, compare values of key and insert in rankedList as for loop iterates
+		// if value matches one in array, move it up? (to first most)
+		return values (favoriteMovies);
+		
+				if (values(i) ) // do count here 
+					
+			rankedList.add(i);		// add into array
+		
+//		String name = value [0];
+		mostHighlyRated ++;
+	}
+	
+}
+	
+	//compare counts at end
+	
+	
+	//insert into array in ascending order
+	
+	
+	
+	
+	
+	
+	
+	// compare with other students fav movies
+	//return ranked list = first in list is more popular among student, last in list is least popular 
+}
+	
+	
+	
+}
+}
